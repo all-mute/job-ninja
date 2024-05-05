a7d6dc347adc13f7e9354f56f5b47abf17dc406e
All Mute ✓ <davidmerkulov@yandex.ru>
2024-05-05 17:58:27 +0300

    /routes/+error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { page } from '$app/stores';
+	import { emojis } from './emojis.js';
+</script>
+
+<h1>Error: {$page.status} {$page.error.message}</h1>
+<span style="font-size: 3em">
+	Ты просишь без уважения...
+</span>
+<span style="font-size: 3em">
+	{emojis[$page.status] ?? emojis[500]}
+</span>
