6b3f8b0fbe2dc157c50f1d5f1564288b22ef2f4e
All Mute ✓ <davidmerkulov@yandex.ru>
2024-05-05 17:04:50 +0300

    schema update, git links

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,6 +1,8 @@
 import { writable } from 'svelte/store';
 
-export const githubURL = writable('https://github.com/engageintellect/project-nexum.git');
+export const githubOrigURL = writable('https://github.com/engageintellect/project-nexum');
+
+export const githubOurURL = writable('https://github.com/all-mute/project-flexin');
 
 export const msgStore = writable('hello world');
 export const feedSelect = writable(false);
