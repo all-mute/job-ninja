04f5b8ff25180c6543f32d953182ccf79842f661
All Mute ✓ <davidmerkulov@yandex.ru>
2024-05-05 18:05:06 +0300

    апдейт ошибки

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,13 @@
 <script>
 	import { page } from '$app/stores';
-	import { emojis } from './emojis.js';
+	import { emojis } from '$lib/emojis.js';
 </script>
 
 <h1>Error: {$page.status} {$page.error.message}</h1>
 <span style="font-size: 3em">
-	Ты просишь без уважения...
+	Ты просишь без уважения... <br />
+	Если что-то действительно сломалось, большая просьба написать tg @nonGilgamesh, мы выдать вам миска рис
 </span>
-<span style="font-size: 3em">
+<span style="font-size: 5em">
 	{emojis[$page.status] ?? emojis[500]}
 </span>
