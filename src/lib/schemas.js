bf930ba41ca5d3e9d258fa1b7a974b8532f34094
All Mute ✓ <davidmerkulov@yandex.ru>
2024-05-07 20:37:00 +0300

    убрал обязательный спец символ из пароля

--- src/lib/schemas.js
+++ src/lib/schemas.js
@@ -1,49 +1,47 @@
 import { z } from 'zod';
 
 export const loginUserSchema = z.object({
-	email: z
-		.string({ required_error: 'Email is required' })
-		.email({ message: 'Email must be a valid email.' }),
-	password: z
-		.string({ required_error: 'Password is required', message: 'Please enter a password.' })
-		.regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
-			message:
-				'Password is required & must be a minimum of 8 characters & contain at least one letter, one number, and one special character.'
-		}),
+  email: z
+    .string({ required_error: 'Email is required' })
+    .email({ message: 'Email must be a valid email.' }),
+  password: z
+    .string({ required_error: 'Password is required', message: 'Please enter a password.' })
+    .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, {
+      message: 'Password is required & must be a minimum of 8 characters & contain at least one letter and one number.'
+    }),
 });
 
 export const registerUserSchema = z
-	.object({
-		email: z
-			.string({ required_error: 'Email is required' })
-			.email({ message: 'Email must be a valid email' }),
-		password: z
-			.string({ required_error: 'Password is required' })
-			.regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
-				message:
-					'Password must be a minimum of 8 characters & contain at least one letter, one number, and one special character.'
-			}),
-		passwordConfirm: z
-			.string({ required_error: 'Confirm Password is required' })
-			.regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
-				message:
-					'Password must be a minimum of 8 characters & contain at least one letter, one number, and one special character.'
-			})
-	})
-	.superRefine(({ passwordConfirm, password }, ctx) => {
-		if (passwordConfirm !== password) {
-			ctx.addIssue({
-				code: z.ZodIssueCode.custom,
-				message: 'Password & Confirm password must match',
-				path: ['password']
-			});
-			ctx.addIssue({
-				code: z.ZodIssueCode.custom,
-				message: 'Password & Confirm password must match',
-				path: ['passwordConfirm']
-			});
-		}
-	});
+  .object({
+    email: z
+      .string({ required_error: 'Email is required' })
+      .email({ message: 'Email must be a valid email' }),
+    password: z
+      .string({ required_error: 'Password is required' })
+      .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, {
+        message: 'Password must be a minimum of 8 characters & contain at least one letter and one number.'
+      }),
+    passwordConfirm: z
+      .string({ required_error: 'Confirm Password is required' })
+      .regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, {
+        message: 'Password must be a minimum of 8 characters & contain at least one letter and one number.'
+      })
+  })
+  .superRefine(({ passwordConfirm, password }, ctx) => {
+    if (passwordConfirm !== password) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Password & Confirm password must match',
+        path: ['password']
+      });
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Password & Confirm password must match',
+        path: ['passwordConfirm']
+      });
+    }
+  });
+
 
 const imageTypes = [
 	'image/jpeg',
@@ -123,16 +121,14 @@ export const updatePasswordSchema = z
 		oldPassword: z.string({ required_error: 'Old password is required' }),
 		password: z
 			.string({ required_error: 'Password is required' })
-			.regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
-				message:
-					'Password must be a minimum of 8 characters & contain at least one letter, one number, and one special character.'
-			}),
+			.regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, {
+				message: 'Password must be a minimum of 8 characters & contain at least one letter and one number.'
+		}),
 		passwordConfirm: z
 			.string({ required_error: 'Confirm Password is required' })
-			.regex(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
-				message:
-					'Password must be a minimum of 8 characters & contain at least one letter, one number, and one special character.'
-			})
+			.regex(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, {
+				message: 'Password must be a minimum of 8 characters & contain at least one letter and one number.'
+		})
 	})
 	.superRefine(({ passwordConfirm, password }, ctx) => {
 		if (passwordConfirm !== password) {
